d481d96f95146cbe9a13702769ebede014575e5c
Eric Erhardt <eric.erhardt@microsoft.com>
2025-10-14 17:18:19 -0500

    Convert to single file app host

--- app_host.cs
+++ app_host.cs
@@ -1,8 +1,11 @@
+#:sdk Aspire.AppHost.Sdk@13.0.0-preview.1.25514.9
+#:package Aspire.Hosting.Python@13.0.0-preview.1.25514.9
+
 #pragma warning disable ASPIREHOSTINGPYTHON001
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddPythonApp("apiservice", "../api_service", "app.py")
+var apiService = builder.AddPythonApp("apiservice", "./api_service", "app.py")
     .WithUvEnvironment()
     .WithExternalHttpEndpoints();
 
